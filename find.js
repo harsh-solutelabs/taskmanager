01721c176cdb35857419f17d78499e3af1d2d353
mehtaharsh929 <mehtaharsh929@gmail.com>
2020-02-27 12:19:26 +0530

    second commit

--- find.js
+++ find.js
@@ -10,14 +10,20 @@ MongoClient.connect(
     }
     const db = client.db(databaseName);
     // db.collection("users").findOne({ name: "Raj", age: 0 }, (error, users) => {
-    db.collection("users").findOne(
-      { _id: new ObjectID("5e5645f824b3521df0d446e1") },
-      (error, users) => {
-        if (error) {
-          return console.log("Unable to Fetch");
-        }
-        console.log(users);
-      }
-    );
+    // db.collection("users").findOne(
+    //   { _id: new ObjectID("5e5645f824b3521df0d446e1") },
+    //   (error, users) => {
+    //     if (error) {
+    //       return console.log("Unable to Fetch");
+    //     }
+    //     console.log(users);
+    //   }
+    // );
+
+    db.collection("tasks")
+      .find({ completed: "true" })
+      .toArray((error, task) => {
+        console.log(task);
+      });
   }
 );
